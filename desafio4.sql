11d6df220c8c8e45d9d5493f693e9d5755107efb
Wendril <wendrild.c.n@gmail.com>
2022-08-04 15:06:19 -0300

    Requisito 4 - Feito

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+SELECT
+	usu.nome AS usuario,
+	IF(YEAR(MAX(rep.data_reproducao)) = 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario 
+FROM
+	SpotifyClone.reproducao AS rep,
+	SpotifyClone.usuario AS usu 
+WHERE
+	usu.usuario_id = rep.usuario_id 
+GROUP BY
+	usu.usuario_id 
+ORDER BY
+	usuario;
